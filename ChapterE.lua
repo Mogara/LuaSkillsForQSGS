7ce3c560251aade6ec1afb05c168fd48c34ec7e6
Fsu0413 <Fsu0413@gmail.com>
2013-07-07 01:15:46 +0800

    Update ChapterE.lua

--- ChapterE.lua
+++ ChapterE.lua
@@ -8,58 +8,66 @@
 	相关武将：一将成名·法正
 	描述：你每次获得一名其他角色两张或更多的牌时，可以令其摸一张牌；每当你受到1点伤害后，你可以令伤害来源选择一项：交给你一张手牌，或失去1点体力。
 	引用：LuaEnyuan
-	状态：验证通过
+	状态：0610验证通过
 ]]--
+
 LuaEnyuan = sgs.CreateTriggerSkill{
-	name = "LuaEnyuan",
-	frequency = sgs.Skill_NotFrequent,
-	events = {sgs.CardsMoveOneTime, sgs.Damaged},
+	name = "LuaEnyuan" ,
+	events = {sgs.CardsMoveOneTime, sgs.Damaged} ,
 	on_trigger = function(self, event, player, data)
 		local room = player:getRoom()
 		if event == sgs.CardsMoveOneTime then
 			local move = data:toMoveOneTime()
-			local dest = move.to
-			local source = move.from
-			if dest then
-				if dest:objectName() == player:objectName() then
-					if source then
-						local cards = move.card_ids
-						local count = cards:length()
-						if count > 1 then
-							if room:askForSkillInvoke(player, self:objectName(), data) then
-								local alives = room:getAlivePlayers()
-								for _,p in sgs.qlist(alives) do
-									if p:objectName() == source:objectName() then
-										room:drawCards(p, 1, self:objectName())
-										break
-									end
-								end
-							end
-						end
+			if move.to and (move.to:objectName() == player:objectName()) and move.from and move.from:isAlive() 
+					and (move.from:objectName() ~= move.to:objectName())
+					and (move.card_ids:length() >= 2)
+					and (move.reason.m_reason ~= sgs.CardMoveReason_S_REASON_PREVIEWGIVE) then
+				local _movefrom
+				for _, p in sgs.qlist(room:getAlivePlayers()) do
+					if move.from:objectName() == p:objectName() then
+						_movefrom = p
+						break
 					end
+				end   --我去MoveOneTime的from居然是player不是splayer，还得枚举所有splayer获取下……
+				_movefrom:setFlags("LuaEnyuanDrawTarget")
+				local invoke = room:askForSkillInvoke(player, self:objectName(), data)
+				_movefrom:setFlags("-LuaEnyuanDrawTarget")
+				if invoke then
+					room:drawCards(_movefrom, 1)
 				end
 			end
 		elseif event == sgs.Damaged then
 			local damage = data:toDamage()
 			local source = damage.from
-			if source then
-				if source:objectName() ~= player:objectName() then
-					local count = damage.damage
-					for i=1, count, 1 do
-						if room:askForSkillInvoke(player, self:objectName(), data) then
-							local card = room:askForExchange(source, self:objectName(), 1, false, "EnyuanGive", true)
-							if card then
-								room:moveCardTo(card, player, sgs.Player_PlaceHand)
-							else
-								room:loseHp(source)
-							end
+			if (not source) or (source:objectName() == player:objectName()) then return false end
+			local x = damage.damage
+			for i = 0, x - 1, 1 do
+				if source:isAlive() and player:isAlive() then
+					if room:askForSkillInvoke(player, self:objectName(), data) then
+						local card
+						if not source:isKongcheng() then
+							card = room:askForExchange(source, self:objectName(), 1, false, "LuaEnyuanGive", true)
 						end
+						if card then
+							local reason = sgs.CardMoveReason(sgs.CardMoveReason_S_REASON_GIVE, source:objectName(),
+															  player:objectName(), self:objectName(), nil)
+							reason.m_playerId = player:objectName()
+							room:moveCardTo(card, source, player, sgs.Player_PlaceHand, reason)
+						else
+							room:loseHp(source)
+						end
+					else
+						break
 					end
+				else
+					break
 				end
 			end
 		end
+		return false
 	end
 }
+
 --[[
 	技能名：恩怨（锁定技）
 	相关武将：怀旧·法正
