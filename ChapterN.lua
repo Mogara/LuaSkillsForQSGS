462b737741a2b2907879a078ec7ec3c7a855b3ed
FrozenFeather <brianvong1003@gmail.com>
2014-08-06 22:41:37 +0800

    Update and Test ChapterN.lua
    Complete ChapterN.lua

--- ChapterN.lua
+++ ChapterN.lua
@@ -7,18 +7,138 @@
 	技能名：逆乱
 	相关武将：1v1·韩遂
 	描述：对手的结束阶段开始时，若其当前的体力值比你大，或其于此回合内对你使用过【杀】，你可以将一张黑色牌当【杀】对其使用。 
+	引用：LuaNiluan、LuaNiluanRecord
+	状态：1217验证通过
 ]]--
+LuaNiluanVS = sgs.CreateOneCardViewAsSkill {
+	name = "LuaNiluan",
+	filter_pattern = ".|black",
+	response_pattern = "@@niluan",
+	view_as = function(slef, card)
+        local slash = sgs.Sanguosha:cloneCard("slash", sgs.Card_SuitToBeDecided, -1)
+        slash:addSubcard(card)
+        slash:setSkillName("LuaNiluan")
+        return slash
+    end,
+}
+LuaNiluan = sgs.CreateTriggerSkill{
+	name = "LuaNiluan",
+	events = {sgs.EventPhaseStart},
+	view_as_skill = LuaNiluanVS,
+	can_trigger = function(self, player)
+		return player:isAlive()
+	end,
+	on_trigger = function(self, event, player, data)
+		if player:getPhase() == sgs.Player_Finish then
+			local room = player:getRoom()
+			local owner = room:findPlayerBySkillName(self:objectName())
+			if owner and owner:objectName()~=player:objectName() and owner:canSlash(player, false) then
+				if player:getHp() > owner:getHp() or not owner:hasFlag("LuaNiluanSlashTarget") then
+					if owner:isKongcheng() then
+						local has_black = false
+						for i=0, 3, 1 do
+							local equip = owner:getEquip(i)
+							if equip and equip:isBlack() then
+								has_black = true
+								break
+							end
+						end
+						if not has_black then return false end
+					end
+					room:setPlayerFlag(owner, "slashTargetFix")
+					room:setPlayerFlag(owner, "slashNoDistanceLimit")
+					room:setPlayerFlag(owner, "slashTargetFixToOne")
+					room:setPlayerFlag(player, "SlashAssignee")
+					local slash = room:askForUseCard(owner, "@@niluan", "@niluan-slash:" .. player:objectName())
+					if slash == nil then
+						room:setPlayerFlag(owner, "-slashTargetFix")
+						room:setPlayerFlag(owner, "-slashNoDistanceLimit")
+						room:setPlayerFlag(owner, "-slashTargetFixToOne")
+						room:setPlayerFlag(player, "-SlashAssignee")
+					end
+				end
+			end
+		end
+		return false
+	end,
+}
+LuaNiluanRecord = sgs.CreateTriggerSkill{
+	name = "#LuaNiluan-record",
+	events = {sgs.TargetConfirmed, sgs.EventPhaseStart},
+	priority = 4,
+	can_trigger = function(self, player)
+		return player ~= nil
+	end,
+	on_trigger = function(self, event, player, data)
+		local room = player:getRoom()
+		if event == sgs.TargetConfirmed then
+			local use = data:toCardUse()
+			if use.from and use.from:objectName() == player:objectName() and use.card:isKindOf("Slash") then
+				for _,to in sgs.qlist(use.to) do
+					if not to:hasFlag("LuaNiluanSlashTarget") then
+						to:setFlags("LuaNiluanSlashTarget")
+					end
+				end
+			end
+		else 
+			if player:getPhase() == sgs.Player_RoundStart then
+				for _,p in sgs.qlist(room:getAlivePlayers()) do
+					p:setFlags("-LuaNiluanSlashTarget")
+				end
+			end
+		end
+		return false
+	end,
+}
 --[[
 	技能名：鸟翔
 	相关武将：阵·蒋钦
 	描述：每当一名角色被指定为【杀】的目标后，若你与此【杀】使用者均与该角色相邻，你可以令该角色须使用两张【闪】抵消此【杀】。 
+	引用：LuaNiaoxiang
+	状态：1217验证通过
 ]]--
+function Table2IntList(thetable)
+	local theqlist = sgs.IntList()
+	for _, p in ipairs(thetable) do
+		theqlist:append(p)
+	end
+	return theqlist
+end
+LuaNiaoxiang = sgs.CreateTriggerSkill{
+	name = "LuaNiaoxiang",
+	events = {sgs.TargetConfirmed},
+	on_trigger = function(self, event, player, data)
+		local room = player:getRoom()
+		local use = data:toCardUse()
+		if use.card:isKindOf("Slash") and use.from:isAlive() then
+			local jink_list = use.from:getTag("Jink_"..use.card:toString()):toIntList()
+			for i=0, use.to:length()-1, 1 do
+				local to = use.to:at(i)
+                if to:isAlive() and to:isAdjacentTo(player) and to:isAdjacentTo(use.from) then
+					local new_data = sgs.QVariant()
+					new_data:setValue(to)			--for AI
+					if room:askForSkillInvoke(player, self:objectName(), new_data) then
+						local jink_table = sgs.QList2Table(jink_list)
+						if jink_list:at(i) == 1 then
+							jink_table[i+1] = 2
+						end
+						jink_list = Table2IntList(jink_table)
+						local list_data = sgs.QVariant()
+						list_data:setValue(jink_list)
+						use.from:setTag("Jink_" .. use.card:toString(), list_data)
+					end
+				end
+			end
+		end
+		return false
+	end,
+}
 --[[
 	技能名：涅槃（限定技）
 	相关武将：火·庞统
 	描述：当你处于濒死状态时，你可以：弃置你区域里所有的牌，然后将你的武将牌翻至正面朝上并重置之，再摸三张牌且体力回复至3点。
 	引用：LuaNiepan、LuaNiepanStart
-	状态：验证通过
+	状态：1217验证通过
 ]]--
 LuaNiepan = sgs.CreateTriggerSkill{
 	name = "LuaNiepan",
