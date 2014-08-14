e732e0bd50ef3ccc1d855a11b17a2466ba433c11
LT-Y <2390918920@qq.com>
2014-08-14 12:53:01 +0800

    Update ChapterW.lua
    updata"LuaWansha"

--- ChapterW.lua
+++ ChapterW.lua
@@ -10,28 +10,40 @@
 	引用：LuaWansha
 	状态：验证通过
 ]]--
-LuaWansha = sgs.CreateTriggerSkill{
+LuaWansha=sgs.CreateTriggerSkill{
 	name = "LuaWansha",
+	events = {sgs.AskForPeaches, sgs.EventPhaseChanging, sgs.Death},
 	frequency = sgs.Skill_Compulsory,
-	events = {sgs.AskForPeaches},
 	on_trigger = function(self, event, player, data)
 		local room = player:getRoom()
-		local current = room:getCurrent()
-		if current:isAlive() then
-			if current:hasSkill(self:objectName()) then
-				local dying = data:toDying()
-				local victim = dying.who
-				local seat = player:getSeat()
-				if current:getSeat() ~= seat then
-					return victim:getSeat() ~= seat
+		if event == sgs.AskForPeaches then
+			local dying = data:toDying()
+			local jiaxu = room:getCurrent()
+			if jiaxu and jiaxu:isAlive() and jiaxu:hasSkill(self:objectName()) and jiaxu:getPhase() ~= sgs.Player_NotActive then
+				if dying.who:objectName() ~= player:objectName() and jiaxu:objectName() ~= player:objectName() then
+					room:setPlayerFlag(player, "Global_PreventPeach")
+				end
+			end
+		else
+			if event == sgs.EventPhaseChanging then
+				local change = data:toPhaseChange()
+				if change.to ~= sgs.Player_NotActive then return false end
+			elseif event == sgs.Death then
+				local death = data:toDeath()
+				if death.who:objectName() ~= player:objectName() or death.who:getPhase() == sgs.Player_NotActive then return false end
+			end
+			for _ , p in sgs.qlist(room:getAllPlayers()) do
+				if p:hasFlag("Global_PreventPeach") then
+                    room:setPlayerFlag(p, "-Global_PreventPeach")
 				end
 			end
 		end
+		return false
 	end,
-	can_trigger = function(self, target)
+	can_trigger = function(self,target)
 		return target
 	end
-}
+	}
 --[[
 	技能名：婉容
 	相关武将：1v1·大乔
