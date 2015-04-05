b6c71297417291a3b73457fcb6b0180b63bfce13
ZY <1254094907@qq.com>
2015-04-05 19:51:20 +0800

    Update ChapterZ.lua

--- ChapterZ.lua
+++ ChapterZ.lua
@@ -394,10 +394,7 @@ LuaZhaoxin = sgs.CreateTriggerSkill{
 ]]--
 LuaZhenlie = sgs.CreateTriggerSkill{
 	name = "LuaZhenlie" ,
-	events = {sgs.TargetConfirmed} ,       
-	can_trigger = function(self, target)
-        	return target ~= nil
-	end,
+	events = {sgs.TargetConfirmed} ,
 	on_trigger = function(self, event, player, data)
 		local room = player:getRoom()
         	if event == sgs.TargetConfirmed then
@@ -405,7 +402,6 @@ LuaZhenlie = sgs.CreateTriggerSkill{
 			if use.to:contains(player) and use.from:objectName() ~= player:objectName() then
 				if use.card:isKindOf("Slash") or use.card:isNDTrick() then
 					if room:askForSkillInvoke(player, self:objectName(), data) then
-						room:broadcastSkillInvoke(self:objectName())
 						player:setFlags("-ZhenlieTarget")
 						player:setFlags("ZhenlieTarget")
 						room:loseHp(player)
