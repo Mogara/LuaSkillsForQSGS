4c43aa2f92749ddf10f802a1683a0a829a58a169
Yanguam Siliagim <Rara@mogara.org>
2015-04-06 09:53:20 +0800

    完杀耦合源码了，这一辈子都不能通过了

--- ChapterV.lua
+++ ChapterV.lua
@@ -73,14 +73,6 @@ Fs在这里多说几句：大家可以对这部分技能尽情测试，要不然
 ]]--
 
 --[[
-	技能名：完杀（锁定技）
-	相关武将：林·贾诩、SP·贾诩
-	描述：在你的回合，除你以外，只有处于濒死状态的角色才能使用【桃】。
-	引用：LuaWansha
-	状态：0610未做（暂时没有想法）
-]]--
-
---[[
 	技能名：纵玄
 	相关武将：一将成名2013·虞翻
 	描述：当你的牌因弃置而置入弃牌堆前，你可以将其中任意数量的牌以任意顺序依次置于牌堆顶。
@@ -1668,5 +1660,47 @@ LuaBingyi = sgs.CreatePhaseChangeSkill{
 			room:askForUseCard(player, "@@LuaBingyi", "@bingyi-card")
 		end
 		return false
+	end
+}
+
+--[[
+	技能名：完杀（锁定技）
+	相关武将：林·贾诩、SP·贾诩
+	描述：在你的回合，除你以外，只有处于濒死状态的角色才能使用【桃】。
+	引用：LuaWansha
+	状态：0405验证失败
+]]--
+LuaWansha=sgs.CreateTriggerSkill{
+	name = "LuaWansha",
+	events = {sgs.AskForPeaches, sgs.EventPhaseChanging, sgs.Death},
+	frequency = sgs.Skill_Compulsory,
+	on_trigger = function(self, event, player, data)
+		local room = player:getRoom()
+		if event == sgs.AskForPeaches then
+			local dying = data:toDying()
+			local jiaxu = room:getCurrent()
+			if jiaxu and jiaxu:isAlive() and jiaxu:hasSkill(self:objectName()) and jiaxu:getPhase() ~= sgs.Player_NotActive then
+				if dying.who:objectName() ~= player:objectName() and jiaxu:objectName() ~= player:objectName() then
+					room:setPlayerFlag(player, "Global_PreventPeach")
+				end
+			end
+		else
+			if event == sgs.EventPhaseChanging then
+				local change = data:toPhaseChange()
+				if change.to ~= sgs.Player_NotActive then return false end
+			elseif event == sgs.Death then
+				local death = data:toDeath()
+				if death.who:objectName() ~= player:objectName() or death.who:getPhase() == sgs.Player_NotActive then return false end
+			end
+			for _ , p in sgs.qlist(room:getAllPlayers()) do
+				if p:hasFlag("Global_PreventPeach") then
+                			 room:setPlayerFlag(p, "-Global_PreventPeach")
+				end
+			end
+		end
+		return false
+	end,
+	can_trigger = function(self,target)
+		return target
 	end
 }
--- ChapterW.lua
+++ ChapterW.lua
@@ -8,42 +8,9 @@
 	相关武将：林·贾诩、SP·贾诩
 	描述：在你的回合，除你以外，只有处于濒死状态的角色才能使用【桃】。
 	引用：LuaWansha
-	状态：1217验证通过
+	状态：0405验证失败
 ]]--
-LuaWansha=sgs.CreateTriggerSkill{
-	name = "LuaWansha",
-	events = {sgs.AskForPeaches, sgs.EventPhaseChanging, sgs.Death},
-	frequency = sgs.Skill_Compulsory,
-	on_trigger = function(self, event, player, data)
-		local room = player:getRoom()
-		if event == sgs.AskForPeaches then
-			local dying = data:toDying()
-			local jiaxu = room:getCurrent()
-			if jiaxu and jiaxu:isAlive() and jiaxu:hasSkill(self:objectName()) and jiaxu:getPhase() ~= sgs.Player_NotActive then
-				if dying.who:objectName() ~= player:objectName() and jiaxu:objectName() ~= player:objectName() then
-					room:setPlayerFlag(player, "Global_PreventPeach")
-				end
-			end
-		else
-			if event == sgs.EventPhaseChanging then
-				local change = data:toPhaseChange()
-				if change.to ~= sgs.Player_NotActive then return false end
-			elseif event == sgs.Death then
-				local death = data:toDeath()
-				if death.who:objectName() ~= player:objectName() or death.who:getPhase() == sgs.Player_NotActive then return false end
-			end
-			for _ , p in sgs.qlist(room:getAllPlayers()) do
-				if p:hasFlag("Global_PreventPeach") then
-                			 room:setPlayerFlag(p, "-Global_PreventPeach")
-				end
-			end
-		end
-		return false
-	end,
-	can_trigger = function(self,target)
-		return target
-	end
-}
+
 --[[
 	技能名：婉容
 	相关武将：1v1·大乔
